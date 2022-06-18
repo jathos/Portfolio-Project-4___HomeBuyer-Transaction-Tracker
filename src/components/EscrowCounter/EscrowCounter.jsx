54fa8947dacb469ef862e97d6c30b5c0eecb7669
jbellon <josh@joshbellon.com>
2022-06-17 23:01:53 -0700

    Escrow counter component's css is slightly improved

--- src/components/EscrowCounter/EscrowCounter.jsx
+++ src/components/EscrowCounter/EscrowCounter.jsx
@@ -14,7 +14,7 @@ function EscrowCounter({ end, start }) {
 
     return (
         <>
-            <h1>Your Escrow Closes In {daysLeftRef.current} Days</h1>
+            <h1 className="escrowStatement">Your Escrow Closes In {daysLeftRef.current} Days</h1>
             <div className="escrowCounter">
                 {daysPassedDivRef.map((ele, idx) => <EscrowDayPassed ele={ele} key={`A${idx}`} />)}
                 {daysLeftDivRef.map((ele, idx) => <EscrowDayLeft ele={ele} key={`B${idx}`} />)}
