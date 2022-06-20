8162706264aafbd38c3ece698e2eab620e614da7
jbellon <josh@joshbellon.com>
2022-06-19 23:08:41 -0700

    html and css framework in place for adding contacts to transaction

--- src/components/ContactsView/ContactsView.jsx
+++ src/components/ContactsView/ContactsView.jsx
@@ -9,11 +9,56 @@ function ContactsView({ user, rerender, setRerender }) {
         <div className="contactsViewWrapper">
             <NewContactForm user={user} rerender={rerender} setRerender={setRerender} />
             <div className="contactsView">
-                <h4>Escrow Officer</h4>
-                <h4>Title Officer</h4>
-                <h4>Mortgage Officer</h4>
-                <h4>Transaction Coordinator</h4>
-                <h4>Vendors</h4>
+                <h4>Escrow Officer</h4><hr></hr>
+                {user.isAdmin ? <><form>
+                    <select></select>
+                    <button>Add</button>
+                </form>
+                </> :
+                    <>
+                        Description
+                    </>
+                }
+                <h4>Title Officer</h4><hr></hr>
+                {user.isAdmin ? <><form>
+                    <select></select>
+                    <button>Add</button>
+                </form>
+                </> :
+                    <>
+                        Description
+                    </>
+                }
+                <h4>Mortgage Officer</h4><hr></hr>
+                {user.isAdmin ? <><form>
+                    <select></select>
+                    <button>Add</button>
+                </form>
+                </> :
+                    <>
+                        Description
+                    </>
+                }
+                <h4>Transaction Coordinator</h4><hr></hr>
+                {user.isAdmin ? <><form>
+                    <select></select>
+                    <button>Add</button>
+                </form>
+                </> :
+                    <>
+                        Description
+                    </>
+                }
+                <h4>Vendors</h4><hr></hr>
+                {user.isAdmin ? <><form>
+                    <select></select>
+                    <button>Add</button>
+                </form>
+                </> :
+                    <>
+                        Description
+                    </>
+                }
             </div>
         </div>
     );
