223eb264cea5a4c93e69646e114080a50f3d8cb5
jbellon <josh@joshbellon.com>
2023-01-19 14:42:34 -0800

    Removed that horrible circular gradient on the background

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -8,7 +8,7 @@ export default function AuthPage({ setUser, rerender, setRerender }) {
     return (
         <main>
             <div className="loginWrapper">
-                <h1 className="welcome">Welome To Your New Home Purchase Management Portal</h1>
+                <h1 className="welcome">Welcome To Your New Home Purchase Management Portal</h1>
                 <button className="loginButton" onClick={() => setShowLogin(!showLogin)}>{showLogin ? 'Sign Up' : 'Log In'}</button>
                 {showLogin ? <LoginForm setUser={setUser} rerender={rerender} setRerender={setRerender} /> : <SignUpForm setUser={setUser} rerender={rerender} />}
             </div>
