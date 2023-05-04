02025c3d0869ae1976d6f73ac7aec431742c7dfc
jbellon <josh@joshbellon.com>
2023-05-04 16:04:47 -0700

    Fixed layout for Sign Up Form

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import { signUp } from '../../utilities/users-service';
+import './SignUpForm.css';
 
 export default class SignUpForm extends Component {
 
@@ -38,18 +39,18 @@ export default class SignUpForm extends Component {
             <div>
                 <div className="form-container">
                     <form className="signupForm" autoComplete="off" onSubmit={this.handleSubmit}>
-                        <label>Name</label>
-                        <input type="text" name="name" value={this.state.name} onChange={this.handleChange} required />
-                        <label>Email</label>
-                        <input type="email" name="email" value={this.state.email} onChange={this.handleChange}
+                        <label className="name-label">Name</label>
+                        <input className="name" type="text" name="name" value={this.state.name} onChange={this.handleChange} required />
+                        <label className="email-label">Email</label>
+                        <input className="email" type="email" name="email" value={this.state.email} onChange={this.handleChange}
                             required />
-                        <label>Password</label>
-                        <input type="password" name="password" value={this.state.password} onChange={this.handleChange}
+                        <label className="password-label">Password</label>
+                        <input className="password" type="password" name="password" value={this.state.password} onChange={this.handleChange}
                             required />
-                        <label>Confirm</label>
-                        <input type="password" name="confirm" value={this.state.confirm} onChange={this.handleChange}
+                        <label className="confirm-label">Confirm</label>
+                        <input className="confirm" type="password" name="confirm" value={this.state.confirm} onChange={this.handleChange}
                             required />
-                        <button type="submit" disabled={disable}>SIGN UP</button>
+                        <button className="signup" type="submit" disabled={disable}>Sign Up</button>
                     </form>
                 </div>
                 <p className="error-message">&nbsp;{this.state.error}</p>
--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -17,7 +17,7 @@ export default function AuthPage({ setUser, rerender, setRerender }) {
             <div className="loginWrapper">
                 <div className="login-text-and-buttons">
                     <h1 className="welcome"><span style={{ fontSize: 80 }}>Welcome</span><br></br> To Your<br></br> <span style={{ fontSize: 90 }}>New Home</span><br></br><span style={{ fontSize: 50 }}>Escrow Management Portal</span></h1>
-                    <button className="login-button" onClick={() => setShowLogin(!showLogin)}>{showLogin ? 'Sign Up' : 'Log In'}</button>
+                    <button className="login-button" onClick={() => setShowLogin(!showLogin)}>{showLogin ? 'New User?  Create an Account' : 'Go Back to Login'}</button>
                     {showLogin ? <LoginForm setUser={setUser} rerender={rerender} setRerender={setRerender} /> : <SignUpForm setUser={setUser} rerender={rerender} />}
                 </div>
                 <div className="login-picture">
