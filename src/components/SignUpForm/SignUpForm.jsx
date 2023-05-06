d990fe188b5c8284e937d49b0962b39e9ef9ac8b
jbellon <josh@joshbellon.com>
2023-05-05 17:28:13 -0700

    Further tweaks to landing page css are now implemented

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -36,7 +36,7 @@ export default class SignUpForm extends Component {
     render() {
         const disable = this.state.password !== this.state.confirm;
         return (
-            <div>
+            <>
                 <div className="form-container">
                     <form className="signupForm" autoComplete="off" onSubmit={this.handleSubmit}>
                         <label className="name-label">Name</label>
@@ -54,7 +54,7 @@ export default class SignUpForm extends Component {
                     </form>
                 </div>
                 <p className="error-message">&nbsp;{this.state.error}</p>
-            </div>
+            </>
         );
     }
 }
