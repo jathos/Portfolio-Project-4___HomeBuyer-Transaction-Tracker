6e768a62386df72c0983b1f1e5f6cd02e298972b
jbellon <josh@joshbellon.com>
2023-05-17 11:58:28 -0700

    Footer component is added to the About and Contact pages.  Padding on About and Contact pages main wrapper is reduced.

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,20 +1,24 @@
+import Footer from '../Footer/Footer';
 import './Contact.css';
 
 function Contact() {
     return (
-        <div className="contact-wrapper">
-            <div className="contact-main">
-                <div className="title-wrapper">
-                    <p className="contact-title">CONTACT</p>
+        <>
+            <div className="contact-wrapper">
+                <div className="contact-main">
+                    <div className="title-wrapper">
+                        <p className="contact-title">CONTACT</p>
+                    </div>
+                    <div className="content-wrapper">
+                        <p className="contact-content"><span class="material-symbols-outlined" style={{ color: '#13097C', position: 'relative', bottom: '14px', right: '3px' }}>
+                            home_app_logo
+                        </span>Transaction Tracker was created by Josh Bellon in Sacramento, CA.  Find him on&nbsp;<a href="https://www.linkedin.com/in/joshbellon/">Linkedin</a>&nbsp;at https://www.linkedin.com/in/joshbellon/</p>
+                    </div>
                 </div>
-                <div className="content-wrapper">
-                    <p className="contact-content"><span class="material-symbols-outlined" style={{ color: '#13097C', position: 'relative', bottom: '14px', right: '3px' }}>
-                        home_app_logo
-                    </span>Transaction Tracker was created by Josh Bellon in Sacramento, CA.  Find him on&nbsp;<a href="https://www.linkedin.com/in/joshbellon/">Linkedin</a>&nbsp;at https://www.linkedin.com/in/joshbellon/</p>
-                </div>
-            </div>
 
-        </div>
+            </div>
+            <Footer></Footer>
+        </>
     )
 };
 
